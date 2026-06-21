fc4060d80620bede3e402358005251e1a355944e
harsh.g <harsh.g@infoedge.com>
2026-06-21 18:48:17 +0530

    refactored the email to username2

--- src/main/java/com/codingshuttle/projects/lovable_clone/dto/auth/UserProfileResponse.java
+++ src/main/java/com/codingshuttle/projects/lovable_clone/dto/auth/UserProfileResponse.java
@@ -3,7 +3,6 @@ package com.codingshuttle.projects.lovable_clone.dto.auth;
 public record UserProfileResponse(
         Long id,
         String username,
-        String name,
-        String avatarUrl
+        String name
 ) {
 }
