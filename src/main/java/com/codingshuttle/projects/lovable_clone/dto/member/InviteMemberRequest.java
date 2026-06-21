78a241913859126c429a0acea5b63ca84f3955d8
harsh.g <harsh.g@infoedge.com>
2026-06-21 15:28:14 +0530

    Added the validation annotations

--- src/main/java/com/codingshuttle/projects/lovable_clone/dto/member/InviteMemberRequest.java
+++ src/main/java/com/codingshuttle/projects/lovable_clone/dto/member/InviteMemberRequest.java
@@ -1,9 +1,16 @@
 package com.codingshuttle.projects.lovable_clone.dto.member;
 
 import com.codingshuttle.projects.lovable_clone.enums.ProjectRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record InviteMemberRequest(
+        @Email
+        @NotBlank
         String email,
+
+        @NotNull
         ProjectRole role
 ) {
 }
